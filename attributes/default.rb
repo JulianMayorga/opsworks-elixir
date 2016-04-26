d77b3bcedee575f2df9a47fd2f52cd2c752c6450
JulianMayorga <mayorga.julian@gmail.com>
2016-04-26 16:24:07 -0300

    Add Gemfile

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,3 +12,14 @@ default[:opsworks_elixir] = {
     }
   }
 }
+
+default['nodejs']['version'] = '5.0.0'
+
+# Override the repo
+case node['platform_family']
+when 'debian'
+  default['nodejs']['repo']      = 'https://deb.nodesource.com/node_0.12'
+end
+
+default["nodebin"]["location"] = '/usr/bin/nodejs'
+default["nodebin"]["opsworks_location"] = '/usr/local/bin/node'
